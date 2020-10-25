15ee71bc9d79d625cf297e8ece1476a5cde07185
DufourC <32374369+Qurmo@users.noreply.github.com>
2020-10-25 09:08:53 +0100

    Update prime.cpp
    Removing double tests, and starting to test from 2 instead of 1.  Since all numbers are divisible by 1 and 1 is the only number that's divisible by one and itself that isn't prime.

--- prime.cpp
+++ prime.cpp
@@ -1,54 +1,36 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> prime_numbers;//This vector will contain all prime numbers found.
+std::vector<int> prime_number;
 
 void prime_List(int a)
 {//Here we test to see if the number is prime.
 	bool prime = true;
-	for(int b = 1; b <= a && prime == true; b++)
+	for(int b = 2; b <= a && prime == true; b++)
 	{
-		if (a % b == 0 && b != a && b != 1)
+		if (a % b == 0 && b != a)
 		{//Set prime to 'false' to stop testing, number is not prime.
 			prime = false;
 		}
-		
-		else
-		{//Keep prime 'true' for further testing.
-			prime = true;
-		}	
-		
-		if(b == a && prime == true)
-		{//Number is prime and can be added to vector. 
-			prime_numbers.push_back(a);//adding 'a' to vector.
-		}		
-	}
-}
 
-void test(int a)
-{//Remove all even numbers from test except 2
-	if(a % 2 == 0 && a != 2)
-	{
-	}
-	
-	else
-	{//Test all odd numbers and 2 for prime
-		prime_List(a);
+		if(b == a)
+		{//Number is prime and can be added to vector.
+			prime_number.push_back(a);
+		}
 	}
 }
 
 int main()
 {
-	int a = 1;//First number to test.
-	int x = 100000;//Till what number to test.
+	int a = 2;//First number to test.
+	int x = 1000;//Till what number to test.
 	while(a <= x)
 	{//here we start the whole testing process.
-		test(a);
+		prime_List(a);
 		a++;
 	}
 	
-	std::cout << "You have found " << prime_numbers.size() << " prime numbers." << std::endl;
+	std::cout << " you have found " << prime_number.size() << " prime numbers." << std::endl;
 
-system("pause");
 return 0;
-}		
+}
